2c346edd393637884e4b11fbae53adb7d35a8237
Muslimbek <muslimbeksalimov0102@gmail.com>
2024-02-26 21:38:23 +0500

    cofff

--- src/Components/Localization/Localization.jsx
+++ src/Components/Localization/Localization.jsx
@@ -206,7 +206,7 @@ const Languages = {
                     oval__h42: "Pick your coffee",
                     oval__text2: "Select from our evolving range of artisan coffees. Our beans are ethically sourced and we pay fair prices for them. There are new coffees in all profiles every month for you to try out.",
     
-                    buttonjon: "Create your plan"
+                    buttonjon: "Create your plan hello"
                 }
             }
     }
